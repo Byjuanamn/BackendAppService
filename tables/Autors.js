bf60c53bb39455d84d70562fd700c7deab1f470b
byjuanamn <juan.antonio.martin@gmail.com>
2016-10-17 12:23:28 +0200

    Lectura es anonima El resto solo para usuarios autenticados

--- tables/Autors.js
+++ tables/Autors.js
@@ -17,8 +17,10 @@ table.columns = {
 * */
 
 
-table.read.access = 'authenticated';
-table.update.access = 'disabled';
+table.read.access = 'anonymous';
+table.update.access = 'authenticated';
+table.delete.access = 'authenticated';
+table.insert.access = 'authenticated';
 
 
 module.exports = table;
